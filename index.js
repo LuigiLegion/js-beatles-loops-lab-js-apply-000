f00fd3705a4beb396591dd6f044524df79e401f9
Tal Luigi <luigi.t.agr@gmail.com>
2018-12-09 21:28:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+	var whoPlaysWhat = [];
+	for(var i = 0; i < musicians.length; i++) {
+		var currentString = musicians[i] + " plays " + instruments[i];
+		whoPlaysWhat.push(currentString);
+	}
+	return whoPlaysWhat;
+}
+
+function johnLennonFacts(facts) {
+	var newFacts = [];
+	var i = 0;
+	while(i < facts.length) {
+		var newFact = facts[i] + "!!!";
+		newFacts.push(newFact);
+		i++;
+	}
+	return newFacts;
+}
